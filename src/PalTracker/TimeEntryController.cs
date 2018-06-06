deca3206899f5cfb91e20e1238d4b53ef56205ef
David Williams <dwilliams459@gmail.com>
2018-06-06 10:47:46 -0600

    Added Health Monitoring

--- src/PalTracker/TimeEntryController.cs
+++ src/PalTracker/TimeEntryController.cs
@@ -7,21 +7,25 @@ namespace PalTracker
     public class TimeEntryController : Controller
     {
         private readonly ITimeEntryRepository _repository;  
+        private readonly IOperationCounter<TimeEntry> _counter;
 
-        public TimeEntryController(ITimeEntryRepository repository)
+        public TimeEntryController(ITimeEntryRepository repository, IOperationCounter<TimeEntry> counter)
         {
             _repository = repository;
+            _counter = counter;
         }
 
         [HttpGet("{id}", Name = "GetTimeEntry")]
         public IActionResult Read(long id)
         {
+            _counter.Increment(TrackedOperation.Read);
             return _repository.Contains(id) ? (IActionResult) Ok(_repository.Find(id)) : NotFound();
         }
 
         [HttpPost]
         public IActionResult Create([FromBody] TimeEntry timeEntry)
         {
+            _counter.Increment(TrackedOperation.Create);
             var newTimeEntry = _repository.Create(timeEntry);
             return CreatedAtRoute("GetTimeEntry", new {id = newTimeEntry.Id}, newTimeEntry);
         }
@@ -29,18 +33,22 @@ namespace PalTracker
         [HttpGet]
         public IActionResult List()
         {
-            return Ok(_repository.List());
+           _counter.Increment(TrackedOperation.List);
+           return Ok(_repository.List());
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(long id, [FromBody] TimeEntry timeEntry)
         {
+            _counter.Increment(TrackedOperation.Update);
             return _repository.Contains(id) ? (IActionResult) Ok(_repository.Update(id, timeEntry)) : NotFound();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+            _counter.Increment(TrackedOperation.Delete);
+
             if (!_repository.Contains(id))
             {
                 return NotFound();
